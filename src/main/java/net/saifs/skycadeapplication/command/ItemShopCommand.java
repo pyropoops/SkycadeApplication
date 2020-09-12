16092504df44fe8f8bde0719ec91c9febd73fe82
Saif <xpyrophoenix21@gmail.com>
2020-09-12 12:36:26 +0100

    convert itemshop to subcommand

--- src/main/java/net/saifs/skycadeapplication/command/ItemShopCommand.java
+++ src/main/java/net/saifs/skycadeapplication/command/ItemShopCommand.java
@@ -2,24 +2,26 @@ package net.saifs.skycadeapplication.command;
 
 import net.saifs.skycadeapplication.SkycadeApplication;
 import net.saifs.skycadeapplication.utils.SAMethods;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class ItemShopCommand implements CommandExecutor {
+public class ItemShopCommand extends SACommand {
+    public ItemShopCommand(String commandName) {
+        super(commandName);
+    }
+
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public void onCommand(CommandSender sender, String[] args) {
         if (!(sender instanceof Player)) {
             SAMethods.send(sender, "&cYou must be a player to do that!");
-            return true;
-        }
-        if (!sender.hasPermission("sa.application.admin") && !sender.isOp()) {
-            SAMethods.send(sender, "&cYou do not have permission to do that!");
-            return true;
+            return;
         }
         Player player = (Player) sender;
         player.openInventory(SkycadeApplication.itemShopGUI.getPage(0));
-        return true;
+    }
+
+    @Override
+    public String getNode() {
+        return "sa.application.admin";
     }
 }
